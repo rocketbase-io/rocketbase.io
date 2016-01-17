d8e5cef3f45c1f9deded739e8af8d152c502d57c
Marten Prieß <marten@non-rocket-science.com>
2016-01-17 08:23:08 +0100

    Update 2016-01-17T07:23:07.309Z

--- js/main.js
+++ js/main.js
@@ -13413,6 +13413,68 @@ parsePositiveInt = function (value, defaultValue) {
             scrollText: '<i class="fa fa-angle-up"></i>',
             scrollTitle: 'Scroll to top'
         });
+
+
+        /**
+         * detect IE
+         * returns version of IE or false, if browser is not Internet Explorer
+         */
+        function detectIE() {
+            var ua = window.navigator.userAgent;
+
+            var msie = ua.indexOf('MSIE ');
+            if (msie > 0) {
+                // IE 10 or older => return version number
+                return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+            }
+
+            var trident = ua.indexOf('Trident/');
+            if (trident > 0) {
+                // IE 11 => return version number
+                var rv = ua.indexOf('rv:');
+                return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+            }
+
+            /*
+             var edge = ua.indexOf('Edge/');
+             if (edge > 0) {
+             // Edge (IE 12+) => return version number
+             return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+             }
+             */
+
+            // other browser
+            return false;
+        }
+
+
+        // detect IE
+        var IEversion = detectIE();
+
+        if (IEversion !== false) {
+            function resizeSvg(maxWidth, maxHeight) {
+                $('.svg-responvie').each(function (i) {
+                    var img = $(this);
+                    var h = img.data('height'), w = img.data('width');
+                    if (w > maxWidth) {
+                        h = h * (maxWidth / w);
+                        w = maxWidth;
+                    }
+                    if (h > maxHeight) {
+                        h = maxHeight;
+                        w = w * (maxHeight / w);
+                    }
+                    img.css('height', h + 'px');
+                    img.css('width', w + 'px');
+                });
+            }
+
+            resizeSvg($('.container').width(), $(window).innerHeight());
+
+            $(window).resize(function () {
+                resizeSvg($('.container').width(), $(window).innerHeight());
+            });
+        }
     });
 
 })(jQuery);
